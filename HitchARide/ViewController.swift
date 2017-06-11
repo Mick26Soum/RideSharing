bd75bd4a3d3893ff1ca16b9bef353891722aaeb0
Mick Soum <Mick26Soum@gmail.com>
2017-06-10 22:30:49 -0700

    Initial Commit

---
+++ HitchARide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HitchARide
+//
+//  Created by MICK SOUMPHONPHAKDY on 6/10/17.
+//  Copyright © 2017 MICK SOUMPHONPHAKDY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
